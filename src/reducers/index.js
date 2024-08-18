3e21505c80bdb5a5895c183f35971fc8ebb72fd5
isterya <vash.vybor.1@gmail.com>
2024-08-18 13:53:43 +0200

    v1.3: added filters by items. Filtering is triggered also when deleting or adding a character

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,9 @@ const initialState = {
    heroes: [],
    heroesLoadingStatus: 'idle',
    filters: [],
+   filtersLoadingStatus: 'idle',
+   activeFilter: 'all',
+   filteredHeroes: [],
 };
 
 const reducer = (state = initialState, action) => {
@@ -16,6 +19,10 @@ const reducer = (state = initialState, action) => {
          return {
             ...state,
             heroes: action.payload,
+            filteredHeroes:
+               state.activeFilter === 'all'
+                  ? action.payload
+                  : action.payload.filter((item) => item.element === state.activeFilter),
             heroesLoadingStatus: 'idle',
          };
 
@@ -25,16 +32,53 @@ const reducer = (state = initialState, action) => {
             heroesLoadingStatus: 'error',
          };
 
-      case 'HERO_DELETED':
+      case 'FILTERS_FETCHING':
          return {
             ...state,
-            heroes: state.heroes.filter((hero) => hero.id !== action.payload),
+            filtersLoadingStatus: 'loading',
+         };
+
+      case 'FILTERS_FETCHED':
+         return {
+            ...state,
+            filters: action.payload,
+            filtersLoadingStatus: 'idle',
+         };
+
+      case 'FILTERS_FETCHING_ERROR':
+         return {
+            ...state,
+            filtersLoadingStatus: 'error',
+         };
+
+      case 'ACTIVE_FILTER_CHANGED':
+         return {
+            ...state,
+            activeFilter: action.payload,
+            filteredHeroes:
+               action.payload === 'all' ? state.heroes : state.heroes.filter((item) => item.element === action.payload),
+         };
+
+      case 'HERO_DELETED':
+         const newHeroList = state.heroes.filter((hero) => hero.id !== action.payload);
+         return {
+            ...state,
+            heroes: newHeroList,
+            filteredHeroes:
+               state.activeFilter === 'all'
+                  ? newHeroList
+                  : newHeroList.filter((item) => item.element === state.activeFilter),
          };
 
       case 'HERO_ADDED':
+         let newAddedHeroList = [...state.heroes, action.payload];
          return {
             ...state,
-            heroes: [...state.heroes, action.payload],
+            heroes: newAddedHeroList,
+            filteredHeroes:
+               state.activeFilter === 'all'
+                  ? newAddedHeroList
+                  : newAddedHeroList.filter((item) => item.element === state.activeFilter),
          };
 
       default:
