a4fb52bcf69c5b9dba299011d779eb39e352fd63
isterya <vash.vybor.1@gmail.com>
2024-08-18 14:21:34 +0200

    v1.4: there is now an animation when removing and reappearing characters

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,10 +1,13 @@
 import { useHttp } from '../../hooks/http.hook';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
 import HeroesListItem from '../heroesListItem/HeroesListItem';
 import Spinner from '../spinner/Spinner';
+
+import './heroesList.scss';
 
 const HeroesList = () => {
    const { filteredHeroes, heroesLoadingStatus } = useSelector((state) => state);
@@ -34,16 +37,22 @@ const HeroesList = () => {
 
    const renderHeroesList = (arr) => {
       if (arr.length === 0) {
-         return <h5 className="text-center mt-5">Героев пока нет</h5>;
+         return (
+            <CSSTransition timeout={0} classNames="hero">
+               <h5 className="text-center mt-5">Героев пока нет</h5>
+            </CSSTransition>
+         );
       }
 
-      return arr.map(({ id, ...props }) => {
-         return <HeroesListItem key={id} {...props} onDelete={() => onDelete(id)} />;
-      });
+      return arr.map(({ id, ...props }) => (
+         <CSSTransition key={id} timeout={500} classNames="hero">
+            <HeroesListItem {...props} onDelete={() => onDelete(id)} />
+         </CSSTransition>
+      ));
    };
 
    const elements = renderHeroesList(filteredHeroes);
-   return <ul>{elements}</ul>;
+   return <TransitionGroup component="ul">{elements}</TransitionGroup>;
 };
 
 export default HeroesList;
